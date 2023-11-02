1088cdf99c252f688358b7e189a9de8ac5a68899
jesseck3013 <jesseck3013@outlook.com>
2023-11-02 23:41:32 +0800

    added enpoints

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const API_BASE_URL = "https://api.football-data.org/v4";
+
+export const AREA = "/area";
+export const COMPETITION = "/competition";
+export const MATCH = "/matches";
+export const PERSON = "/persons";
