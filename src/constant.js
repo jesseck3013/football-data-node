41c13183652a911f8ef35b3e1ec49303208ddfd2
jesseck3013 <jesseck3013@outlook.com>
2023-11-03 04:07:01 +0800

    correct endpoint path

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = "https://api.football-data.org/v4";
 
-export const AREA = "/area";
-export const COMPETITION = "/competition";
+export const AREA = "/areas";
+export const COMPETITION = "/competitions";
 export const MATCH = "/matches";
 export const PERSON = "/persons";
