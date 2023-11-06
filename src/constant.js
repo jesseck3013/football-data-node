a070d3e1549d47c663015a74fec2a419e2ddf9f0
jesseck3013 <jesseck3013@outlook.com>
2023-11-07 00:59:58 +0800

    added TEAM endpoint constant

--- src/constant.js
+++ src/constant.js
@@ -3,4 +3,5 @@ export const API_BASE_URL = "https://api.football-data.org/v4";
 export const AREA = "/areas";
 export const COMPETITION = "/competitions";
 export const MATCH = "/matches";
+export const TEAM = "/teams";
 export const PERSON = "/persons";
